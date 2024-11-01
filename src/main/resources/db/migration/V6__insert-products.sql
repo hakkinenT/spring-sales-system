d9c4c582611d4cf06769ab89052082be328bb5ca
Tawanna <thol_16@hotmail.com>
2024-10-31 21:15:29 -0300

    chore: adiciona migration para popular a tabela produto

---
+++ src/main/resources/db/migration/V6__insert-products.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_product(name, price) VALUES ('Apple iPhone 15', 4859.10);
+INSERT INTO tb_product(name, price) VALUES ('Smartphone Samsung Galaxy S23', 2998.80);
+INSERT INTO tb_product(name, price) VALUES ('Notebook Apple Macbook Air', 8549.99);
+INSERT INTO tb_product(name, price) VALUES ('Notebook Samsung Galaxy Book Go', 1699.00);
+INSERT INTO tb_product(name, price) VALUES ('Smart TV 58" 4K UHD D-LED Philips', 2374.05);
