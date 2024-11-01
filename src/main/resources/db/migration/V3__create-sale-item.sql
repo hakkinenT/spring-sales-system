e002b6a696d1b609e54df271cdd6d9c21b339240
Tawanna <thol_16@hotmail.com>
2024-11-01 12:20:36 -0300

    fix: remove NOT NULL da especificação do campo total da tabela Item de Venda

--- src/main/resources/db/migration/V3__create-sale-item.sql
+++ src/main/resources/db/migration/V3__create-sale-item.sql
@@ -2,7 +2,7 @@ CREATE TABLE tb_sale_item(
 	product_id BIGINT NOT NULL,
 	sale_id BIGINT NOT NULL,
 	quantity INTEGER DEFAULT 0,
-	subtotal NUMERIC(10, 2) NOT NULL DEFAULT 0.00,
+	subtotal NUMERIC(10, 2) DEFAULT 0.00,
 	PRIMARY KEY(product_id, sale_id),
 	FOREIGN KEY (product_id) REFERENCES tb_product(id),
 	FOREIGN KEY (sale_id) REFERENCES tb_sale(id)
